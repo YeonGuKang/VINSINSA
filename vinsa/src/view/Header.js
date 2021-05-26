78d440e067ccf24d3d3f57618c733919be6073e5
YeonGuKang <hsmroof@naver.com>
2021-05-27 01:33:32 +0900

    no message

--- vinsa/src/view/Header.js
+++ vinsa/src/view/Header.js
@@ -82,7 +82,7 @@ const Header = () => {
     // 로그인시 일반사용자 , 운영자를 구분
   
     <div>
-       <h1><Link to="/">VINSINSA</Link></h1>
+       <h1 className={menu.logo2}><Link to="/">VINSINSA</Link></h1>
      {isLoggedIn ? IsManager ? <h>관리자입니다.</h> : state ? <h>판매자입니다.</h> : <h>구매자입니다.</h> : null}
     <div className={menu.header}>
         <div className={menu.Rlogo}>
