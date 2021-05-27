7a03d2366d31a452c21ef1e6ee4e611108156f4d
BitnaKeum <beausty23@naver.com>
2021-05-27 13:19:17 +0900

    MyInfo 완성

--- vinsa/src/view/Myinfo.js
+++ vinsa/src/view/Myinfo.js
@@ -26,6 +26,7 @@ const [My_info, setMy_info] = useState([]);
       // url로 넘어온 본인 state를 얻음
       mystate = window.location.href.split("/")[4]
       mystate=decodeURI(mystate)
+        let num = 1
 
       if(mystate == "운영자")
       {
@@ -33,6 +34,7 @@ const [My_info, setMy_info] = useState([]);
         dbService.collection("user").onSnapshot((snapshot) => {
             const boardArray = snapshot.docs.map((doc) => ({
               id: doc.id,
+              num: num++,
               ...doc.data(),
             }));
             setAll_info(boardArray)
@@ -74,43 +76,54 @@ const [My_info, setMy_info] = useState([]);
         <Header></Header>
         <div className="backwrap">
         <div className="mainform">
-        <table>
+
+    <h2 align="center">{IsManager ? "유저 정보 조회하기" : "내 정보 조회하기"}</h2>
+    { IsManager ? null :
+    <table width="463px" height="300px" border="1px">
+      <tbody>
+        <tr>
+          <td width="100px"><h4>&nbsp;아이디</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.id}</h>}</td>
+        </tr>
+        <tr>
+          <td><h4>&nbsp;이메일</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.email}</h>}</td>
+        </tr>
+        <tr>
+          <td><h4>&nbsp;생일</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.birth}</h>}</td>
+        </tr>
+        <tr>
+          <td><h4>&nbsp;이름</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.name}</h>}</td>
+        </tr>
+        <tr>
+          <td><h4>&nbsp;성별</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.sex}</h>}</td>
+        </tr>
+        <tr>
+          <td><h4>&nbsp;주소</h4></td><td>{IsManager ? null : <h>&nbsp;{My_info.address}</h>}</td>
+        </tr>
+      </tbody>
+    </table> }
+
+
+    <h3>&nbsp;총 회원 수 : {All_info.length}</h3>
+    <h5>&nbsp;회원 관리 시 회원아이디를 클릭하세요.</h5>
+    <table width="463px" height="300px" border="1px">
       <thead>
         <tr>
-          <th>Lorem</th><th>Ipsum</th><th>Dolor</th>
+          <th>회원 번호</th><th>아이디</th>
         </tr>
       </thead>
       <tbody>
+      {All_info.map((Show)=>(
         <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
+          <td width="120px" align="center"><h4>&nbsp;{Show.num}</h4></td><td align="center">&nbsp;<Link to={"/Userinfo"+"/"+Show.id}>{Show.id}</Link></td>
         </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
-        <tr>
-          <td>Lorem</td><td>Ipsum</td><td>Dolor</td>
-        </tr>
+      ))}
+
       </tbody>
     </table>
-        {IsManager ? null : <h3>아이디 : {My_info.id}</h3>}
-        {IsManager ? null :<h3>이메일 :  {My_info.email}</h3>}
-        {IsManager ? null :<h3>생일 :  {My_info.birth}</h3>}
-        {IsManager ? null :<h3>이름 :  {My_info.name}</h3>}
-        {IsManager ? null :<h3>성별 :  {My_info.sex}</h3>}
-        {IsManager ? null :<h3>주소 :  {My_info.address}</h3>}
-        {IsManager ? null :<h3>{My_info.state}</h3>}
-    
-        {IsManager ? <h1>회원 수 : {All_info.length}</h1>: null}
-        {IsManager ? <h5>회원관리시 회원아이디 클릭</h5>: null}
-        {All_info.map((Show)=>(
-                <li>아이디 : <Link to={"/Userinfo"+"/"+Show.id}>{Show.id}</Link></li>
-         ))}
+
+
+
+
 
        
           <div className="authBtns">
